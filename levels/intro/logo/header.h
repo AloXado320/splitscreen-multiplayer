97eedd0d0296168404d92b05a750769ab00634c4
sunlitspace542 <rabbidsluis123@gmail.com>
2020-11-28 21:21:04 -0500

    New title logo

--- levels/intro/logo/header.h
+++ levels/intro/logo/header.h
@@ -1,11 +1,19 @@
-extern Vtx logo_Logo_008_mesh_vtx_0[476];
-extern Gfx logo_Logo_008_mesh_tri_0[];
-extern Vtx logo_Logo_008_mesh_vtx_1[496];
-extern Gfx logo_Logo_008_mesh_tri_1[];
-extern Vtx logo_Logo_008_mesh_vtx_2[124];
-extern Gfx logo_Logo_008_mesh_tri_2[];
-extern Vtx logo_Logo_008_mesh_vtx_3[276];
-extern Gfx logo_Logo_008_mesh_tri_3[];
+extern Vtx logo_logo_mesh_vtx_0[243];
+extern Gfx logo_logo_mesh_tri_0[];
+extern Vtx logo_logo_mesh_vtx_1[1351];
+extern Gfx logo_logo_mesh_tri_1[];
+extern Vtx logo_logo_mesh_vtx_2[256];
+extern Gfx logo_logo_mesh_tri_2[];
+extern Vtx logo_logo_mesh_vtx_3[233];
+extern Gfx logo_logo_mesh_tri_3[];
+extern Vtx logo_logo_mesh_vtx_4[179];
+extern Gfx logo_logo_mesh_tri_4[];
+extern Vtx logo_logo_mesh_vtx_5[133];
+extern Gfx logo_logo_mesh_tri_5[];
+extern Vtx logo_logo_mesh_vtx_6[300];
+extern Gfx logo_logo_mesh_tri_6[];
+extern Vtx logo_logo_mesh_vtx_7[262];
+extern Gfx logo_logo_mesh_tri_7[];
 
-extern Gfx logo_Logo_008_mesh[];
+extern Gfx logo_logo_mesh[];
 
