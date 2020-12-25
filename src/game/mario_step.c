b321fa1e78aa4b74ff05d034a6d6dbfefae6b26e
sunlitspace542 <rabbidsluis123@gmail.com>
2020-12-25 12:46:25 -0500

    Fix Invisible Walls!

--- src/game/mario_step.c
+++ src/game/mario_step.c
@@ -272,7 +272,7 @@ static s32 perform_ground_quarter_step(struct MarioState *m, Vec3f nextPos) {
     upperWall = resolve_and_return_wall_collisions(nextPos, 60.0f, 50.0f);
 
     floorHeight = find_floor(nextPos[0], nextPos[1], nextPos[2], &floor);
-    ceilHeight = vec3f_find_ceil(nextPos, floorHeight, &ceil);
+    ceilHeight = vec3f_find_ceil(nextPos, nextPos[1], &ceil);
 
     waterLevel = find_water_level(nextPos[0], nextPos[2]);
 
